a622d2df399fa26484a769c8ce047dccfdf3cc1f
Enigmatic331 <unreal_legend7@hotmail.com>
2017-09-07 23:40:57 +0800

    Upload all files

---
+++ WindowsApplication2/Forms/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+   Public Sub SetMessage(ByVal strMessage As String)
+      Label1.Text = strMessage
+      Label1.Refresh()
+   End Sub
+
+   Private Sub Form1_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+      Me.Text = System.Diagnostics.Process.GetCurrentProcess().ProcessName
+   End Sub
+End Class
---
+++ WindowsApplication2/Modules/Module1.vb
@@ -0,0 +1,157 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.IO
+Imports System.Text
+Imports System.IO.IsolatedStorage
+
+
+Module Module1
+    Dim CookieJar As New CookieContainer
+
+    Sub Main()
+        Dim arrArg() As String
+        arrArg = System.Environment.GetCommandLineArgs()
+
+        Call Form1.Show()
+
+        If arrArg.Length >= 2 Then
+            Call Routine(arrArg(1))
+        Else
+            MsgBox("WTF?")
+        End If
+    End Sub
+
+
+    Sub Routine(ByVal strFileName As String)
+        Dim sURL As String, strToken As String, strOriLine As String
+        Dim intCount As Integer, intPos As Integer
+        Dim objStream As Stream
+
+        strFileName = Replace(strFileName, "=", "")
+
+        Try
+            'get file extension  
+            strToken = GetCurrentToken(strFileName)
+            sURL = "http://www.lazada.com.my/ajax/lottery/spinTheWheel/?lang=en&platform=desktop&wheelToken=" & strToken & "&dpr=" & Trim(strFileName)
+            'sURL = "http://www.lazada.com.my/ajax/campaign/play/?lang=en&platform=desktop&dpr=" & Trim(strFileName)
+
+            intCount = 0
+            While True
+                intCount = intCount + 1
+                Call Form1.SetMessage("Voucher Miner Processing Count - " & intCount)
+
+                If intCount > 10000 Then
+                    strToken = GetCurrentToken(strFileName)
+                    sURL = "http://www.lazada.com.my/ajax/lottery/spinTheWheel/?lang=en&platform=desktop&wheelToken=" & strToken & "&dpr=" & Trim(strFileName)
+                    'sURL = "http://www.lazada.com.my/ajax/campaign/play/?lang=en&platform=desktop&dpr=" & Trim(strFileName)
+                    intCount = 0
+                End If
+
+
+                Dim myHttpWebRequest As HttpWebRequest = CType(WebRequest.Create(sURL), HttpWebRequest)
+                Dim res As HttpWebResponse
+                myHttpWebRequest.CookieContainer = CookieJar
+
+
+                myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0"
+                myHttpWebRequest.Referer = "http://www.lazada.com.my/shopping-kaw-kaw"
+                myHttpWebRequest.Host = "www.lazada.com.my"
+                myHttpWebRequest.Accept = "*/*"
+                myHttpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
+
+                'Try
+                objStream = myHttpWebRequest.GetResponse.GetResponseStream
+                res = myHttpWebRequest.GetResponse
+                SaveIncomingCookies(res, Trim(strFileName))
+                'Catch e As WebException
+                '   MsgBox(e.Response)
+                'End Try
+
+
+                Dim objReader As New StreamReader(objStream)
+                Dim sLine As String = ""
+
+                Do While Not sLine Is Nothing
+                    sLine = objReader.ReadLine
+                    If Not sLine Is Nothing Or Trim(sLine) <> "" Then
+                        'extract sLine
+                        strOriLine = sLine
+                        intPos = InStr(sLine, """productsBlockTitle"":", CompareMethod.Text)
+                        sLine = sLine.Substring(intPos + Len("""productsBlockTitle"":"))
+                        intPos = InStr(sLine, """", CompareMethod.Text)
+                        sLine = sLine.Remove(IIf(intPos - 1 >= 0, intPos - 1, 0))
+                        sLine = Replace(sLine, """", "")
+
+                        If sLine <> "Exclusive Early Deals! Don't miss out!" Then
+                            'intPos = 0
+                            'intPos = 1 / intPos
+                            System.IO.File.AppendAllText("C:\Lazada\" & strFileName & ".txt", strOriLine & vbCrLf)
+                            res.Cookies(0).Expires = DateTime.Now
+                            res.Cookies(1).Expires = DateTime.Now
+                            res.Cookies(2).Expires = DateTime.Now
+                            Exit Sub
+                        End If
+                    End If
+                Loop
+
+                'objReader = Nothing
+            End While
+        Catch ex As Exception
+            Exit Sub
+        End Try
+
+    End Sub
+
+    Private Function GetCurrentToken(strFileName) As String
+        Dim sURL As String, sToken As String
+        'Dim wrGETURL As WebRequest
+        Dim objStream As Stream
+        Dim intPos As Integer
+
+        sURL = "http://www.lazada.com.my/ajax/lottery/settings/?lang=en&platform=desktop&dpr=" & Trim(strFileName)
+        'sURL = "http://www.lazada.com.my/ajax/campaign/play/?lang=en&platform=desktop&dpr=" & Trim(strFileName)
+        Dim res As HttpWebResponse
+        Dim myHttpWebRequest As HttpWebRequest = CType(WebRequest.Create(sURL), HttpWebRequest)
+        myHttpWebRequest.CookieContainer = CookieJar
+        myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0"
+        myHttpWebRequest.Referer = "http://www.lazada.com.my/shopping-kaw-kaw"
+        myHttpWebRequest.Host = "www.lazada.com.my"
+        myHttpWebRequest.Accept = "*/*"
+        myHttpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
+
+        objStream = myHttpWebRequest.GetResponse.GetResponseStream()
+        res = myHttpWebRequest.GetResponse
+        SaveIncomingCookies(res, Trim(strFileName))
+
+
+        Dim objReader As New StreamReader(objStream)
+        Dim sLine As String = ""
+        sLine = objReader.ReadLine
+        'extract wheeltoken
+        intPos = InStr(sLine, "{""wheelToken"":", CompareMethod.Text)
+        sLine = sLine.Substring(intPos + Len("{""wheelToken"":"))
+        intPos = InStr(sLine, """", CompareMethod.Text)
+        sLine = sLine.Remove(intPos - 1)
+        sToken = sLine
+
+        'clear all cookies
+        res.Cookies(0).Expires = DateTime.Now
+        res.Cookies(1).Expires = DateTime.Now
+        res.Cookies(2).Expires = DateTime.Now
+        res.Cookies(3).Expires = DateTime.Now
+        res.Cookies(4).Expires = DateTime.Now
+        res.Cookies(5).Expires = DateTime.Now
+
+        Return sToken
+    End Function
+
+    Private Sub SaveIncomingCookies(ByRef response As HttpWebResponse, ByVal dpr As Integer)
+        If response.Headers("Set-Cookie") <> Nothing Then
+            'CookieJar.SetCookies(New Uri("http://www.lazada.com.my/ajax/campaign/play/?lang=en&platform=desktop&dpr=" & dpr), response.Headers("Set-Cookie"))
+            CookieJar.SetCookies(New Uri("http://www.lazada.com.my/ajax/lottery/play/?lang=en&platform=desktop&dpr=" & dpr), response.Headers("Set-Cookie"))
+        End If
+    End Sub
+End Module
